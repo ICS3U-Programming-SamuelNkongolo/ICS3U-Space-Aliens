2cc6ea06d25636a512046fd1da0a47dc9c6cbb93
Samuel Nkongolo <samuel.nkongolo@stu.ocsb.ca>
2023-01-10 18:50:28 +0000

    hello world

---
+++ code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created by: Samuel Nkongolo
+# Created on: Jan, 9 2023
+#This program is the "Space Aliens" program on PyBadge
+
+#import ugame
+#import stage
+
+def game_scene():
+    #this function is the main game game_scene
+
+    print("\n\n\n") #3 blank lines
+    print("Hello, World!")
+
+    #repeat forever, game loop
+    while True:
+        pass #just a place holder
+
+if __name__ == "__main__":
+    game_scene()
