934cabf397291399fb84f9345ae4a8255e534f94
Samuel Nkongolo <samuel.nkongolo@stu.ocsb.ca>
2023-01-13 15:24:13 +0000

    code.py

--- code.py
+++ code.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
-# Created by: Samuel Nkongolo
-# Created on: Jan, 9 2023
+#Created by: Samuel Nkongolo
+#Created on: Jan, 9 2023
 #This program is the "Space Aliens" program on PyBadge
 
-#import ugame
-#import stage
+import ugame
+import stage
+
 
 def game_scene():
-    #this function is the main game game_scene
+        # Gets image from file (16x16) and sets it as the stage.
+    image_bank_background = stage.Bank.from_bmp16
+    ("space_aliens_background.bmp")
 
-    print("\n\n\n") #3 blank lines
-    print("Hello, World!")
+    # Display the inmage background
+    background = stage.Grid(image_bank_background, 10, 8)
 
-    #repeat forever, game loop
+    # Displays the image stage background
+    # 60 Frames Per Sec (FPS)
+
+    game = stage.Stage(ugame.display, 60)
+
+    #list of layers for the game (In order left appear first)
+    game.layers = [background]
+
+    # Renders all sprites
+    # Renders the background
+    game.render_block()
+
+    # Placeholder
     while True:
-        pass #just a place holder
+        pass
 
 if __name__ == "__main__":
     game_scene()
---
+++ constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: October 2019
+# This constants file is for Space Alien game
+
+# CircuitPython screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+TOTAL_NUMBER_OF_LASERS = 5
+SHIP_SPEED = 1
+ALIEN_SPEED = 1
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+LASER_SPEED = 2
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
+
